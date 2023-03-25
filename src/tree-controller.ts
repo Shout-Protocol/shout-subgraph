077f25ac84202f41e2a132dee1235378f7276832
Sainy <sainy@mytrinket.xyz>
2023-03-25 17:33:05 +0700

    completed v0.0.3

--- src/tree-controller.ts
+++ src/tree-controller.ts
@@ -1,74 +1,158 @@
 import {
-  Approval as ApprovalEvent,
-  DelegateChanged as DelegateChangedEvent,
-  DelegateVotesChanged as DelegateVotesChangedEvent,
-  Transfer as TransferEvent
+  TreeAdded, TreeAudited, TreeTransferred, TreeWithdrew
 } from "../generated/TreeController/TreeController"
 import {
-  Approval,
-  DelegateChanged,
-  DelegateVotesChanged,
-  Transfer
+  TreeNFT
+} from "../generated/TreeController/TreeNFT"
+import {
+  Tree,
+  Report,
+  Owner,
+  Auditor,
+  NFT,
 } from "../generated/schema"
+import { Address, BigInt, Bytes } from "@graphprotocol/graph-ts";
 
-export function handleApproval(event: ApprovalEvent): void {
-  let entity = new Approval(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.owner = event.params.owner
-  entity.spender = event.params.spender
-  entity.value = event.params.value
+export function handleTreeAdded(event: TreeAdded): void {
+  const id = event.params.nftAddress.toHex() + '-' + event.params.tokenId.toHex()
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  let tree = Tree.load(id);
+  let owner = getOrCreateOwner(event.params.owner);
 
-  entity.save()
+  if (!tree) {
+    tree = new Tree(id)
+    tree.owner = owner.id
+    tree.nftAddress = event.params.nftAddress
+    tree.tokenId = event.params.tokenId
+    tree.treeNumber = event.params.treeNumber
+    tree.isActive = true
+    tree.nft = getOrCreateNFT(event.params.nftAddress, event.params.tokenId).id
+
+    tree.createdAt = event.block.timestamp
+    tree.createdTxHash = event.transaction.hash
+    tree.updatedAt = event.block.timestamp
+    tree.updatedTxHash = event.transaction.hash
+  } else {
+    tree.owner = owner.id
+    tree.treeNumber = event.params.treeNumber
+
+    tree.isActive = true
+    tree.updatedAt = event.block.timestamp
+    tree.updatedTxHash = event.transaction.hash
+  }
+
+  owner.totalTrees = owner.totalTrees.plus(event.params.treeNumber)
+
+  owner.save()
+  tree.save()
 }
 
-export function handleDelegateChanged(event: DelegateChangedEvent): void {
-  let entity = new DelegateChanged(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.delegator = event.params.delegator
-  entity.fromDelegate = event.params.fromDelegate
-  entity.toDelegate = event.params.toDelegate
+export function handleTreeTransferred(event: TreeTransferred): void {
+  const id = event.params.nftAddress.toHex() + '-' + event.params.tokenId.toHex()
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  let tree = Tree.load(id);
+  let oldOwner = getOrCreateOwner(event.params.from);
+  let newOwner = getOrCreateOwner(event.params.to);
 
-  entity.save()
+  if (tree) {
+    tree.owner = event.params.to;
+
+    tree.updatedAt = event.block.timestamp
+    tree.updatedTxHash = event.transaction.hash
+
+    oldOwner.totalTrees = oldOwner.totalTrees.minus(tree.treeNumber)
+    newOwner.totalTrees = newOwner.totalTrees.plus(tree.treeNumber)
+
+    tree.save()
+  }
+
+  oldOwner.save()
+  newOwner.save()
 }
 
-export function handleDelegateVotesChanged(
-  event: DelegateVotesChangedEvent
+export function handleTreeWithdrew(
+  event: TreeWithdrew
 ): void {
-  let entity = new DelegateVotesChanged(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.delegate = event.params.delegate
-  entity.previousBalance = event.params.previousBalance
-  entity.newBalance = event.params.newBalance
+  const id = event.params.nftAddress.toHex() + '-' + event.params.tokenId.toHex()
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  const tree = Tree.load(id);
 
-  entity.save()
+  if (tree) {
+    const owner = getOrCreateOwner(tree.owner as Address)
+    owner.totalTrees = owner.totalTrees.minus(tree.treeNumber)
+
+    tree.owner = Address.zero()
+    tree.isActive = false
+
+    owner.save()
+    tree.save()
+  }
 }
 
-export function handleTransfer(event: TransferEvent): void {
-  let entity = new Transfer(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.from = event.params.from
-  entity.to = event.params.to
-  entity.value = event.params.value
+export function handleTreeAudited(event: TreeAudited): void {
+  const treeId = event.params.nftAddress.toHex() + '-' + event.params.tokenId.toHex()
+  const tree = Tree.load(treeId);
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  const reportId = event.transaction.hash.toHex();
+  const report = new Report(reportId);
 
-  entity.save()
+  report.auditor = getOrCreateAuditor(event.params.auditor).id
+  report.treeNumber = event.params.treeNumber
+  report.timestamp = event.block.timestamp
+  report.transactionHash = event.transaction.hash
+
+  if (tree) {
+    const owner = getOrCreateOwner(tree.owner as Address)
+
+    if (event.params.treeNumber.gt(tree.treeNumber)) {
+      const diff = event.params.treeNumber.minus(tree.treeNumber)
+      owner.totalTrees = owner.totalTrees.plus(diff)
+    } else {
+      const diff = tree.treeNumber.minus(event.params.treeNumber)
+      owner.totalTrees = owner.totalTrees.minus(diff)
+    }
+
+    report.tree = tree.id
+
+    tree.save()
+    owner.save()
+  }
+
+  report.save()
+}
+
+export function getOrCreateOwner(id: Address): Owner {
+  let owner = Owner.load(id);
+  if (owner == null) {
+    owner = new Owner(id);
+    owner.totalTrees = BigInt.fromI32(0);
+    owner.save();
+  }
+  return owner;
+}
+
+export function getOrCreateAuditor(id: Address): Auditor {
+  let auditor = Auditor.load(id);
+  if (auditor == null) {
+    auditor = new Auditor(id);
+    auditor.save();
+  }
+  return auditor;
+}
+
+export function getOrCreateNFT(address: Address, tokenId: BigInt): NFT {
+  let id = address.toHex() + "-" + tokenId.toHex()
+  let nft = NFT.load(id);
+  if (nft == null) {
+    nft = new NFT(id);
+
+    let instance = TreeNFT.bind(address);
+    let tokenUri = instance.try_tokenURI(tokenId)
+    if (!tokenUri.reverted) {
+      nft.tokenUri = tokenUri.value
+    }
+
+    nft.save();
+  }
+  return nft;
 }
